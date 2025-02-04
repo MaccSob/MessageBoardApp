f24b1978a7f666793290c767aae111cf9ca2fe61
MaccSob <71sobmaciej@gmail.com>
2025-02-04 23:54:49 +0100

    setting up things

---
+++ server.js
@@ -0,0 +1,40 @@
+import http from 'http';
+import path from 'path';
+
+const PORT = process.env.PORT;
+
+
+const server = http.createServer(async(req, res) => {
+
+    try {
+        // if get request
+        if (req.method ='GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+           
+               } else if(req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+               } else {
+                   throw new Error('Not found');
+               }
+               const data = await fs.readFile(filePath);
+               res.setHeader('Content-type', 'text/html');
+               res.write(data);
+               res.end();
+                    } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'})
+        res.end("<h2>Server error</h2>");
+    }
+
+
+
+    
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

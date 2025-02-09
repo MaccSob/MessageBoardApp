45c852fd310c8dd362ade23d1f926d2029274fe0
MaccSob <71sobmaciej@gmail.com>
2025-02-10 00:34:28 +0100

    loops index

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-});
+app.set('view engine', 'ejs');
 
-app.get('/log', (req, res) => {
-  res.send('Log Page')
-})
+
+app.get('/', (req, res) => {
+    res.render('index')
+ });
+ app.get('/new', (req, res) => {
+    res.render('new')
+ })
+
 
 app.listen(3000, () => console.log('Server Started'))
--- views/app.js
+++
@@ -1,5 +0,0 @@
-const path = require("node:path");
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-

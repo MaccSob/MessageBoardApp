a78d671edb22cb3cb97e7517f19e62664c06efde
MaccSob <71sobmaciej@gmail.com>
2025-02-12 23:55:15 +0100

    routing

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) => {
-    res.render('index')
- });
- app.get('/new', (req, res) => {
-    res.render('new')
- })
 
 
-app.listen(3000, () => console.log('Server Started'))
+
+const server = express();
+
+
+
+server.listen(8000, () => console.log('Server Started'))
